d38c97ffb553449d8713d16b9f4069c4df73b06f
Konrad Lis <o.konradlis@gmail.com>
2019-12-10 11:08:59 +0100

    Add title of each option

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import OrderSummary from '../Ordersummary/OrderSummary.js';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption.js';
 
 const OrderForm = (props) => (
   //console.log('orderformProps', props),
+  //console.log('tablepricing',pricing),
+
+  
   <Grid>
     <Row>
+      {pricing.map(option => 
+        <Col md={4} key={option.id}>
+          <OrderOption title={option.name} />
+          {/*{console.log('eachOption', option)}*/}
+        </Col>)}
       <Col xs={12}>
         <OrderSummary cost={props.tripCost} options={props.options}/>
       </Col>
