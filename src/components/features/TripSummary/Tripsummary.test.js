f2fe5296480eb89d3ac334a637b1fc77bd5d5266
Konrad Lis <o.konradlis@gmail.com>
2019-12-16 17:23:32 +0100

    Add unit test tags

--- src/components/features/TripSummary/Tripsummary.test.js
+++ src/components/features/TripSummary/Tripsummary.test.js
@@ -5,20 +5,44 @@ import TripSummary from './TripSummary';
 describe('Component TripSummary', () => {
   it('should generate correct adress /trip/abc', () => {
     const id = 'abc';
-    const component = shallow(<TripSummary id={id}/>);
+    const component = shallow(<TripSummary tags={[]}
+      id={id}/>);
 
-    expect(component.find('.link').prop('to').toEqual('/trip/${id}'));
+    expect(component.find('Link').prop('to')).toEqual(`/trip/${id}`);
   });
 
   it('should <src> have correct src and alt', () => {
-    const exSrc = 'image';
-    const exAlt = 'imagetext';
-    const component = shallow(<TripSummary image={exSrc} name={exAlt} />);
+    const imageSrc = 'image';
+    const imageAlt = 'imagetext';
+    const component = shallow(<TripSummary tags={[]} image={imageSrc} name={imageAlt} />);
 
-    expect(component.find('img').prop('src').toEqual('exSrc'));
-    expect(component.find('img').prop('alt').toEqual('exAlt'));
+    expect(component.find('img').prop('src')).toEqual(imageSrc);
+    expect(component.find('img').prop('alt')).toEqual(imageAlt);
   });
   it('should render correct props name, cost, days', () => {
+    const component = shallow(<TripSummary tags={[]} name={'correctname'} cost={'correctcost'} days={7}/>);
+
+    expect(component).toBeTruthy();
+    // console.log(component.debug());
+  });
+
+  it('should throw error if id, image, name, const, days props are missing', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+
+  it('it should render [tags] with correct sequences', () => {
+    const tagsArray = ['sec', 'min', 'hour'];
+    const component = shallow(<TripSummary tags={tagsArray} />);
+
+    expect(component.find('.tags span').at(0).text()).toEqual(tagsArray[0]);
+    expect(component.find('.tags span').at(1).text()).toEqual(tagsArray[1]);
+    expect(component.find('.tags span').at(2).text()).toEqual(tagsArray[2]);
+    //console.log(component.debug());
+  });
+  it('div.tags it should not shown when tags.array is empty or undefined', () => {
+    const component = shallow(<TripSummary tags={[]} />);
     
+    expect(component.find('div.tags').isEmpty()).toEqual(true);
+    console.log(component.debug());
   });
 });
